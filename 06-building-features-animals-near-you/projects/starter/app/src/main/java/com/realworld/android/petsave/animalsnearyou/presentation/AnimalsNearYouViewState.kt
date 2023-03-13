5d5dc8dc406cef5090870ebd50f97f59322ba4b4
AnthonyMwanzia <anthony.mwanzia@mgas.ke>
2023-03-13 20:56:11 +0300

    Build Features - Animal Near You

--- 06-building-features-animals-near-you/projects/starter/app/src/main/java/com/realworld/android/petsave/animalsnearyou/presentation/AnimalsNearYouViewState.kt
+++ 06-building-features-animals-near-you/projects/starter/app/src/main/java/com/realworld/android/petsave/animalsnearyou/presentation/AnimalsNearYouViewState.kt
@@ -1,2 +1,11 @@
 package com.realworld.android.petsave.animalsnearyou.presentation
 
+import com.realworld.android.petsave.common.presentation.Event
+import com.realworld.android.petsave.common.presentation.model.UIAnimal
+
+data class AnimalsNearYouViewState(
+    val loading: Boolean = true, // 1
+    val animals: List<UIAnimal> = emptyList(), // 2
+    val noMoreAnimalsNearby: Boolean = false, // 3
+    val failure: Event<Throwable>? = null // 4
+)
